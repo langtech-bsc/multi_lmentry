20c83ece87c3133c974687363170c5208017cee6
Andrew-Wyn <terraformer.144@gmail.com>
2025-10-27 16:09:18 +0100

    created structure for package

---
+++ lmentry/version.py
@@ -0,0 +1,8 @@
+_MAJOR = "0"
+_MINOR = "0"
+# On main and in a nightly release the patch should be one ahead of the last
+# released build.
+_PATCH = "1"
+
+VERSION_SHORT = "{0}.{1}".format(_MAJOR, _MINOR)
+VERSION = "{0}.{1}.{2}".format(_MAJOR, _MINOR, _PATCH)
